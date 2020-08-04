134a69ebf05d97ea392b1eea80979ba8bddb0f52
Daniel Hill <dan@mamu.co>
2020-08-04 12:08:37 +0100

    add basic structure for logic
    Signed-off-by: Daniel Hill <dan@mamu.co>

--- src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/ReconciliationApplication.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/ReconciliationApplication.kt
@@ -6,13 +6,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ReconciliationApplication(private val hbaseConnection: Connection, private val metadatastoreConnection: java.sql.Connection): CommandLineRunner {
-	override fun run(vararg args: String?) {
-		println("$hbaseConnection")
-		println("$metadatastoreConnection")
-	}
+class ReconciliationApplication(
+    private val hbaseConnection: Connection,
+    private val metadatastoreConnection: java.sql.Connection
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        println("$hbaseConnection")
+        println("$metadatastoreConnection")
+        fetchUnreconciledRecords().forEach {
+            if (recordExistsInHbase(it)) {
+                reconcileRecord()
+            }
+        }
+    }
+
+    // retrieve records from metadata store
+    private fun fetchUnreconciledRecords(): Array<String> {
+        return arrayOf("a", "b")
+    }
+
+    // check for items in HBase
+    private fun recordExistsInHbase(record: String): Boolean {
+        return true
+    }
+
+    // If found then update metadata store
+    private fun reconcileRecord() {
+
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<ReconciliationApplication>(*args)
+    runApplication<ReconciliationApplication>(*args)
 }
