d5e9f22899e919f2a1dfd4798323b290b28d30f7
Daniel Hill <dan@mamu.co>
2020-08-07 15:53:47 +0100

    add pattern to TextUtils.kt
    Signed-off-by: Daniel Hill <dan@mamu.co>

--- src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/configuration/TextUtils.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/configuration/TextUtils.kt
@@ -1,12 +1,20 @@
 package uk.gov.dwp.dataworks.kafkatohbase.reconciliation.configuration
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
 class TextUtils {
 
-    private val qualifiedTablePattern = Regex(Config.Hbase.qualifiedTablePattern)
+    @Value("\${hbase.table.pattern}")
+    private lateinit var qualifiedTablePattern: String
+
+//    private val qualifiedTablePattern = """^\w+\.([-\w]+)\.([-\w]+)$"""
+
     private val coalescedNames = mapOf("agent_core:agentToDoArchive" to "agent_core:agentToDo")
 
-    fun topicNameTableMatcher(topicName: String) = qualifiedTablePattern.find(topicName)
+    fun topicNameTableMatcher(topicName: String) = Regex(qualifiedTablePattern).find(topicName)
 
-    fun coalescedName(tableName: String) =
-        if (coalescedNames[tableName] != null) coalescedNames[tableName] else tableName
+    fun coalescedName(tableName: String) = tableName
+//        if (coalescedNames[tableName] != null) coalescedNames[tableName] else tableName
 }
