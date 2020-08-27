069a550c3a150a82ee0ebf3e196ab43fe2b99660
Daniel Hill <dan@mamu.co>
2020-08-27 13:05:19 +0100

    add DataWorks logging library update some of the config vars and their defaults
    Signed-off-by: Daniel Hill <dan@mamu.co>

--- src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/configuration/HbaseConfiguration.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/configuration/HbaseConfiguration.kt
@@ -1,30 +1,48 @@
 package uk.gov.dwp.dataworks.kafkatohbase.reconciliation.configuration
 
-import org.apache.hadoop.conf.Configuration
+
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.HConstants
 import org.apache.hadoop.hbase.client.Connection
 import org.apache.hadoop.hbase.client.ConnectionFactory
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import uk.gov.dwp.dataworks.logging.DataworksLogger
 
-@org.springframework.context.annotation.Configuration
+@Configuration
 @ConfigurationProperties(prefix = "hbase")
 data class HbaseConfiguration(
     var zookeeperParent: String? = null,
     var zookeeperQuorum: String? = null,
-    var zookeeperPort: String? = null,
-    var clientScannerTimeoutPeriod: String? = null,
-    var clientScannerCaching: String? = null
-
+    var timeoutMs: String? = null,
+    var clientTimeoutMs: String? = null,
+    var rpcReadTimeoutMs: String? = null
 ) {
 
-    fun hbaseConfiguration(): Configuration = Configuration().apply {
-        set(HConstants.ZOOKEEPER_ZNODE_PARENT, zookeeperParent ?: "/hbase")
-        set(HConstants.ZOOKEEPER_QUORUM, zookeeperQuorum ?: "localhost")
-        setInt("hbase.zookeeper.port", zookeeperPort?.toIntOrNull() ?: 2181)
-        setInt("hbase.client.scanner.timeout.period", clientScannerTimeoutPeriod?.toIntOrNull() ?: 60000)
-        setInt("hbase.client.scanner.caching", clientScannerCaching?.toIntOrNull() ?: 1)
+    companion object {
+        val logger = DataworksLogger.getLogger(HbaseConfiguration::class.toString())
+    }
+
+    fun hbaseConfiguration(): org.apache.hadoop.conf.Configuration {
+
+        val configuration = org.apache.hadoop.conf.Configuration().apply {
+            set(HConstants.ZOOKEEPER_ZNODE_PARENT, zookeeperParent ?: "/hbase")
+            set(HConstants.ZOOKEEPER_QUORUM, zookeeperQuorum ?: "localhost")
+            setInt("hbase.zookeeper.port", 2181)
+            setInt(HConstants.HBASE_CLIENT_SCANNER_TIMEOUT_PERIOD, timeoutMs?.toIntOrNull() ?: 1800000)
+            setInt(HConstants.HBASE_CLIENT_OPERATION_TIMEOUT, clientTimeoutMs?.toIntOrNull() ?: 3600000)
+            setInt(HConstants.HBASE_RPC_READ_TIMEOUT_KEY, rpcReadTimeoutMs?.toIntOrNull() ?: 1800000)
+        }
+
+        logger.info(
+            "Timeout configuration",
+            "scanner" to configuration.get(HConstants.HBASE_CLIENT_SCANNER_TIMEOUT_PERIOD),
+            "rpc" to configuration.get(HConstants.HBASE_RPC_READ_TIMEOUT_KEY),
+            "client" to configuration.get(HConstants.HBASE_CLIENT_OPERATION_TIMEOUT)
+        )
+
+        return configuration
     }
 
     @Bean
@@ -32,6 +50,8 @@ data class HbaseConfiguration(
 
         val connection = ConnectionFactory.createConnection(HBaseConfiguration.create(hbaseConfiguration()))
         addShutdownHook(connection)
+
+        logger.info("Established connection with Hbase")
 
         return connection
     }
