e9b13684cf0c4b5aea85d31975fd434ce13b7e7e
Daniel Chicot <dan.chicot@gmail.com>
2020-09-09 17:35:02 +0100

    Unit test for grouped records.

--- src/test/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/repositories/impl/MetadataStoreRepositoryImplTest.kt
+++ src/test/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/repositories/impl/MetadataStoreRepositoryImplTest.kt
@@ -1,13 +1,36 @@
 package uk.gov.dwp.dataworks.kafkatohbase.reconciliation.repositories.impl
 
 import com.nhaarman.mockitokotlin2.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.sql.Connection
-import java.sql.PreparedStatement
-import java.sql.ResultSet
-import java.sql.Statement
+import java.sql.*
 
 class MetadataStoreRepositoryImplTest {
+
+    @Test
+    fun testReconciledRecords() {
+        val resultSet = mock<ResultSet> {
+            on { next() } doReturnConsecutively (1..101).map {it < 100}
+            on { getInt("id") } doReturnConsecutively  (1..100).toList()
+            on { getString("topic_name") } doReturnConsecutively (1..100).map { "db.database.collection${it % 3}" }
+            on { getString("hbase_id") } doReturnConsecutively (1..100).map { "hbase_id_$it" }
+            on { getTimestamp(("hbase_timestamp"))} doReturnConsecutively (1..100).map { Timestamp(it.toLong()) }
+        }
+
+        val statement = mock<PreparedStatement> {
+            on { executeQuery() } doReturn resultSet
+        }
+
+        val metadataStoreConnection = mock<Connection> {
+            on { prepareStatement(any()) } doReturn statement
+        }
+
+        val metadataStoreRepository = MetadataStoreRepositoryImpl(metadataStoreConnection, "ucfs")
+        val grouped = metadataStoreRepository.groupedUnreconciledRecords()
+        assertEquals(3, grouped.size)
+        grouped.keys.sorted().forEachIndexed { index, key -> assertEquals("db.database.collection$index", key) }
+        grouped.forEach { (_, records) -> assertEquals(33, records.size) }
+    }
 
     @Test
     fun givenALimitExistsForRecordsReturnedWhenIRequestAListOfRecordsFromMetadataStoreThenTheFirstValueContainsLimit() {
