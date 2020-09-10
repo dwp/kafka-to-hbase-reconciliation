42d71d5cdf03ea57002220a908b95a1cae98fe1a
Mike Winker <mike.winker@infinityworks.com>
2020-09-10 19:22:47 +0100

    Update use of active profiles for hbase

--- src/test/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/utils/TableNameUtilTest.kt
+++ src/test/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/utils/TableNameUtilTest.kt
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.mock.mockito.SpyBean
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = [TableNameUtil::class])
+@ActiveProfiles("HBASE")
 class TableNameUtilTest {
 
     @SpyBean
