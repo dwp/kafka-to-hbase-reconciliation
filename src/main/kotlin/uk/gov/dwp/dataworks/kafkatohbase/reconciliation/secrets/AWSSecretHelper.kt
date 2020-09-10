243cfeb861d753de90261388102509acf22b0d44
Daniel Chicot <dan.chicot@gmail.com>
2020-09-10 18:42:24 +0100

    Ensure secretmanagerclient is managed by spring.

--- src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/secrets/AWSSecretHelper.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/secrets/AWSSecretHelper.kt
@@ -1,31 +1,27 @@
 package uk.gov.dwp.dataworks.kafkatohbase.reconciliation.secrets
 
+import com.amazonaws.services.secretsmanager.AWSSecretsManager
 import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
-import uk.gov.dwp.dataworks.kafkatohbase.reconciliation.configuration.SecretsManagerConfiguration
 import uk.gov.dwp.dataworks.kafkatohbase.reconciliation.services.ScheduledReconciliationService
 import uk.gov.dwp.dataworks.logging.DataworksLogger
 
 @Component
-class AWSSecretHelper : SecretHelperInterface {
+class AWSSecretHelper(private val secretsManager: AWSSecretsManager) : SecretHelperInterface {
 
     companion object {
         val logger = DataworksLogger.getLogger(ScheduledReconciliationService::class.toString())
     }
 
-    val configuration: SecretsManagerConfiguration = SecretsManagerConfiguration()
-
     override fun getSecret(secretName: String): String? {
-
-        val client = configuration.secretsManagerClient()
 
         logger.info("Getting value from aws secret manager", "secret_name" to secretName)
 
         try {
             val getSecretValueRequest = GetSecretValueRequest().withSecretId(secretName)
 
-            val getSecretValueResult = client.getSecretValue(getSecretValueRequest)
+            val getSecretValueResult = secretsManager.getSecretValue(getSecretValueRequest)
 
             logger.debug("Successfully got value from aws secret manager", "secret_name" to secretName)
 
