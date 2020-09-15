021ed271ac2c53c6753c09412b6f7b726b71fb8e
Daniel Chicot <dan.chicot@gmail.com>
2020-09-15 17:28:45 +0100

    Extra logging to aide hbase connection issue in qa. (#16)
    Extra logging to aide hbase connection issue in qa.

--- src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/repositories/impl/HBaseRepositoryImpl.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/repositories/impl/HBaseRepositoryImpl.kt
@@ -23,25 +23,17 @@ class HBaseRepositoryImpl(private val connection: Connection, private val tableN
             .map { it.first }
             .toList()
 
-    override fun recordExistsInHBase(topicName: String, id: String, version: Long) =
-        if (connection.admin.tableExists(table(topicName))) {
-            (connection.getTable(table(topicName))).use {
-                it.exists(get(id, version))
-            }
-        } else {
-            logger.warn("Table does not exist", "hbase_table_name" to (tableName(topicName) ?: ""))
-            false
-        }
-
-    private fun recordsExistInHBase(topicName: String, records: List<UnreconciledRecord>): List<Pair<UnreconciledRecord, Boolean>> =
-        if (connection.admin.tableExists(table(topicName))) {
+    private fun recordsExistInHBase(topicName: String, records: List<UnreconciledRecord>): List<Pair<UnreconciledRecord, Boolean>> {
+        logger.info("Checking batch is in hbase", "topic_name" to topicName, "table" to "${table(topicName)}")
+        return if (connection.admin.tableExists(table(topicName))) {
             (connection.getTable(table(topicName))).use { table ->
-                records.zip(table.existsAll(records.map {get(it.hbaseId, it.version)}).asIterable())
+                records.zip(table.existsAll(records.map { get(it.hbaseId, it.version) }).asIterable())
             }
         } else {
-            logger.warn("Table does not exist, marking all as not in hbase", "table_name" to (tableName(topicName) ?: ""))
+            logger.warn("Table does not exist, marking all as not in hbase","table_name" to (tableName(topicName) ?: ""))
             records.map { Pair(it, false) }
         }
+    }
 
     private fun get(id: String, version: Long) = Get(tableNameUtil.decodePrintable(id)).apply { setTimeStamp(version) }
     private fun table(topicName: String) = TableName.valueOf(tableName(topicName))
