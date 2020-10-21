bcbf87dfd13213dbea481d22fedf1e0c886961fb
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2020-10-21 09:08:37 +0100

    Add meta timeout

--- src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/configuration/HBaseConfiguration.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/kafkatohbase/reconciliation/configuration/HBaseConfiguration.kt
@@ -19,6 +19,7 @@ data class HBaseConfiguration(
     var zookeeperPort: String? = "NOT_SET",
     var clientScannerTimeoutPeriodMs: String? = "NOT_SET",
     var clientOperationTimeoutMs: String? = "NOT_SET",
+    var clientMetaOperationTimeoutMs: String? = "NOT_SET",
     var rpcTimeoutMs: String? = "NOT_SET",
     var rpcReadTimeoutMs: String? = "NOT_SET",
     var retries: String? = "NOT_SET",
@@ -37,6 +38,7 @@ data class HBaseConfiguration(
             setInt("hbase.zookeeper.port", zookeeperPort?.toIntOrNull() ?: 666)
             setInt(HConstants.HBASE_CLIENT_SCANNER_TIMEOUT_PERIOD, clientScannerTimeoutPeriodMs?.toIntOrNull() ?: 666)
             setInt(HConstants.HBASE_CLIENT_OPERATION_TIMEOUT, clientOperationTimeoutMs?.toIntOrNull() ?: 666)
+            setInt(HConstants.HBASE_CLIENT_META_OPERATION_TIMEOUT, clientMetaOperationTimeoutMs?.toIntOrNull() ?: 666)
             setInt(HConstants.HBASE_RPC_TIMEOUT_KEY, rpcTimeoutMs?.toIntOrNull() ?: 666)
             setInt(HConstants.HBASE_RPC_READ_TIMEOUT_KEY, rpcReadTimeoutMs?.toIntOrNull() ?: 666)
             setInt(HConstants.HBASE_CLIENT_RETRIES_NUMBER, retries?.toIntOrNull() ?: 666)
@@ -48,7 +50,8 @@ data class HBaseConfiguration(
             "scanner" to configuration.get(HConstants.HBASE_CLIENT_SCANNER_TIMEOUT_PERIOD),
             "rpc" to configuration.get(HConstants.HBASE_RPC_TIMEOUT_KEY),
             "rpc_read" to configuration.get(HConstants.HBASE_RPC_READ_TIMEOUT_KEY),
-            "client" to configuration.get(HConstants.HBASE_CLIENT_OPERATION_TIMEOUT))
+            "client" to configuration.get(HConstants.HBASE_CLIENT_OPERATION_TIMEOUT),
+            "client_meta" to configuration.get(HConstants.HBASE_CLIENT_META_OPERATION_TIMEOUT))
 
         logger.info("HBase Configuration loaded", "hbase_configuration" to configuration.toString())
         return configuration
